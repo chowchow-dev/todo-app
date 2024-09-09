28e32a107b7d968149c855463a450e2761de40ba
chowchow-dev <phillipng.dev@gmail.com>
2024-09-09 17:37:25 +0700

    add github deployment ci

--- vite.config.js
+++ vite.config.js
@@ -6,28 +6,27 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [vue(), mode === 'development' && vueDevTools()].filter(Boolean),
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      }
-    },
-    css: {
-      modules: {
-        generateScopedName: mode === 'production' ? '[local]_[hash:base64:5]' : '[local]'
-      }
-    },
-    test: {
-      globals: true,
-      environment: 'jsdom',
-      root: fileURLToPath(new URL('./', import.meta.url)),
-      coverage: {
-        provider: 'v8',
-        include: ['**/*.{js,vue}'],
-        reporter: ['text', 'json', 'html']
-      }
+export default defineConfig(({ mode }) => ({
+  base: '/vue-todo-app/',
+  plugins: [vue(), mode === 'development' && vueDevTools()].filter(Boolean),
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    modules: {
+      generateScopedName: mode === 'production' ? '[local]_[hash:base64:5]' : '[local]'
+    }
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    root: fileURLToPath(new URL('./', import.meta.url)),
+    coverage: {
+      provider: 'v8',
+      include: ['**/*.{js,vue}'],
+      reporter: ['text', 'json', 'html']
     }
   }
-})
+}))
