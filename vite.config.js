5ec004523bccbd9720af05c062917aa6202dbfee
chowchow-dev <phillipng.dev@gmail.com>
2024-09-10 09:29:33 +0700

    fix ci

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/todo-app/',
+  base: '/',
   plugins: [vue(), mode === 'development' && vueDevTools()].filter(Boolean),
   resolve: {
     alias: {
